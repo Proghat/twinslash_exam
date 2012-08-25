7d04301a07a2e365428450a61f0935ce032386cb
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2012-08-25 11:34:22 +0300

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwinslashExam::Application.config.secret_token = 'c6ea6380d069f1c5aaa798b998ef2e556e52a9df8892145a7951db6a7cff46c8dec6eb656c3f2394eb85b20369079aed298ad655a87b58b49f4238b486e03d8f'
