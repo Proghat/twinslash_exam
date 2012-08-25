9282ebbca1236efbd5f26dbc23d5e1b4733961ab
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2012-08-25 17:10:05 +0300

    User has secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :role
+  attr_accessible :name, :email, :password, :password_confirmation, :role
+  has_secure_password
+
   has_many :orders
 
   validates :name, presence: true, length: { maximum: 50 }
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: {with: EMAIL_REGEX},
     uniqueness: {case_sensitive: false}
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120825095554) do
+ActiveRecord::Schema.define(:version => 20120825121337) do
 
   create_table "orders", :force => true do |t|
     t.string   "title"
@@ -25,8 +25,9 @@ ActiveRecord::Schema.define(:version => 20120825095554) do
     t.string   "name"
     t.string   "email"
     t.string   "role"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
 end
