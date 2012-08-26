d62caa7658d2a1f9f49a858537928bae8156ecfb
Dmitry Zhlobo <dima.zhlobo@gmail.com>
2012-08-26 23:57:19 +0300

    Add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+admin = User.create name: 'Vasily Pupkin', email: 'pupkin@mail.ru',
+  password: 'pupkin', password_confirmation: 'pupkin', role: 'admin'
+user = User.create name: 'Ivan Ivanov', email: 'd.ivan@mail.ru',
+  password: 'ivanushka', password_confirmation: 'ivanushka', role: 'user'
+
+user.orders.create title: 'First order', description: 'Simple first order',
+  completion: false
+user.orders.create title: 'Completed order', description: 'This is completed order',
+  completion: true
+user.orders.create title: 'Uncompleted order', description: 'This is uncompleted order',
+  completion: false
+admin.orders.create title: 'Admin order', description: 'Order created by admin',
+  completion: false
